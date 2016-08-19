37b373a38e0393a6d72853d75661f3996266d653
NathanDevelops97 <nathandevelops@gmail.com>
2016-08-19 10:35:38 -0400

    Updated code. Added a player array/data model

--- SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
+++ SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
@@ -1,7 +1,40 @@
-//: Playground - noun: a place where people can play
+// Nate Grant: Soccer Coordinator Project - 01
 
-import UIKit
+/*
+ 
+ PART 1: Choose an appropriate data type to store information for each player (Name, Team, Guardian Name, Practice Date/Time).
+ 
+ Next, create an empty collection variable to hold all the players’ data.
+ 
+ 
+ PART 2: Assign all 18 players to teams, experience level on all teams bust be the same. Store each teams player in its own new colllection variable.
+ 
+ 
+ PART 3: Create Logic that generates a letter for all of the players guardians, letting them know which team their child has been placed on and when they should attend their first team team practice.
+ 
+ Data: (player name, team name, guardians’ names, practice date/time)
+ 
+ Practice Times (Dragons - March 17, 1pm, Sharks - March 17, 3pm, Raptors - March 18, 1pm)
+ 
+*/
 
-var str = "Hello, playground"
 
-// Nate Grant Just Made A Comment
+// PART 1 - Create a data type to store player information (Name, Parents Names, Experience, Height)
+
+
+
+
+// DATA FOR TOTAL PLAYERS
+
+let numberOfPlayers: Int = 18
+
+
+
+
+
+// Individual Players
+
+let joeSmith = ["name" : "John Smith", "experience" : "yes", "height" : "42", "guardians" : "Jim and Jan Smith"]
+
+var playerArray = [joeSmith]
+
