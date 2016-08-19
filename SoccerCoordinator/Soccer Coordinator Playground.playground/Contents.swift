923fb784e323ff794cf2ce6eb71dd30c67c9ad95
NathanDevelops97 <nathandevelops@gmail.com>
2016-08-19 16:32:37 -0400

    Added player data and arrays

--- SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
+++ SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
@@ -28,13 +28,45 @@
 
 let numberOfPlayers: Int = 18
 
-
-
-
-
 // Individual Players
 
-let joeSmith = ["name" : "John Smith", "experience" : "yes", "height" : "42", "guardians" : "Jim and Jan Smith"]
+let player1 = ["name" : "John Smith", "experience" : "yes", "height" : "42", "guardians" : "Jim and Jan Smith"]
 
-var playerArray = [joeSmith]
+let player2 = ["name" : "Jill Tanner", "experience" : "yes", "height" : "36", "guardians" : "Clara Tanner"]
+
+let player3 = ["name" : "Bill Bon", "experience" : "yes", "height" : "43", "guardians" : "Sara and Jenny Bon"]
+
+let player4 = ["name" : "Eva Gordon", "experience" : "no", "height" : "45", "guardians" : "Wendy and Mike Gordon"]
+
+let player5 = ["name" : "Matt Gill", "experience" : "no", "height" : "40", "guardians" : "Charles and Sylvia Gill"]
+
+let player6 = ["name" : "Kimmy Stein", "experience" : "no", "height" : "41", "guardians" : "Bill and Hillary Stein"]
+
+let player7 = ["name" : "Sammy Adams", "experience" : "no", "height" : "45", "guardians" : "Jeff Adams"]
+
+let player8 = ["name" : "Karl Saygan", "experience" : "yes", "height" : "42", "guardians" : "Heather Bledsoe"]
+
+let player9 = ["name" : "Suzane Greenberg", "experience" : "yes", "height" : "44", "guardians" : "Henrietta Dumas"]
+
+let player10 = ["name" : "Sal Dali", "experience" : "no", "height" : "41", "guardians" : "Gala Dali"]
+
+let player11 = ["name" : "Diego Soto", "experience" : "yes", "height" : "41", "guardians" : "Robin and Sarika Soto"]
+
+let player12 = ["name" : "Ben Finkelstein", "experience" : "no", "height" : "44", "guardians" : "Aaron and Jill Finkelstein"]
+
+let player13 = ["name" : "Arnold Willis", "experience" : "no", "height" : "43", "guardians" : "Claire Willis"]
+
+let player14 = ["name" : "Phillip Helm", "experience" : "yes", "height" : "44", "guardians" : "Thomas Helm and Eva Jones"]
+
+let player15 = ["name" : "Les Clay", "experience" : "yes", "height" : "42", "guardians" : "Wynonna Brown"]
+
+let player16 = ["name" : "Herschel Krustofski", "experience" : "yes", "height" : "45", "guardians" : "Hyman and Rachel Krustofski"]
+
+let player17 = ["name" : "Chloe Alaska", "experience" : "no", "height" : "47", "guardians" : "David and Jamie Alaska"]
+
+let player18 = ["name" : "Joe Kavalier", "experience" : "no", "height" : "39", "guardians" : "Sam and Elaine Kavalier"]
+
+var playerArray = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+
 
