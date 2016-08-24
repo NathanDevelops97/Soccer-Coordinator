164f15d33d68108422bb36bc202241e348f2d788
Nathanael Grant <nathandevelops@gmail.com>
2016-08-23 20:55:21 -0700

    Using Key Value pairs and re-evaluating my code and talking with other developers to improve and focus on what I'm learning!

--- SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
+++ SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
@@ -21,7 +21,7 @@
 
 
 
-
+import Foundation
 
 
 
@@ -31,109 +31,38 @@
 // PART 1: Choose an appropriate data type to store information for each player (Name, Team, Guardian Name, Practice Date/Time).
 
 
-// Individual Players
+// Individual Players. Created a collection to hold all of the players data
 
 
-let player1 = ["name" : "John Smith", "soccerExperience" : "yes", "height" : "42", "guardians" : "Jim and Jan Smith"]
-
-let player2 = ["name" : "Jill Tanner", "soccerExperience" : "yes", "height" : "36", "guardians" : "Clara Tanner"]
-
-let player3 = ["name" : "Bill Bon", "soccerExperience" : "yes", "height" : "43", "guardians" : "Sara and Jenny Bon"]
-
-let player4 = ["name" : "Eva Gordon", "soccerExperience" : "no", "height" : "45", "guardians" : "Wendy and Mike Gordon"]
-
-let player5 = ["name" : "Matt Gill", "soccerExperience" : "no", "height" : "40", "guardians" : "Charles and Sylvia Gill"]
-
-let player6 = ["name" : "Kimmy Stein", "soccerExperience" : "no", "height" : "41", "guardians" : "Bill and Hillary Stein"]
-
-let player7 = ["name" : "Sammy Adams", "soccerExperience" : "no", "height" : "45", "guardians" : "Jeff Adams"]
-
-let player8 = ["name" : "Karl Saygan", "soccerExperience" : "yes", "height" : "42", "guardians" : "Heather Bledsoe"]
-
-let player9 = ["name" : "Suzane Greenberg", "soccerExperience" : "yes", "height" : "44", "guardians" : "Henrietta Dumas"]
-
-let player10 = ["name" : "Sal Dali", "soccerExperience" : "no", "height" : "41", "guardians" : "Gala Dali"]
-
-let player11 = ["name" : "Diego Soto", "soccerExperience" : "yes", "height" : "41", "guardians" : "Robin and Sarika Soto"]
-
-let player12 = ["name" : "Ben Finkelstein", "soccerExperience" : "no", "height" : "44", "guardians" : "Aaron and Jill Finkelstein"]
-
-let player13 = ["name" : "Arnold Willis", "soccerExperience" : "no", "height" : "43", "guardians" : "Claire Willis"]
-
-let player14 = ["name" : "Phillip Helm", "soccerExperience" : "yes", "height" : "44", "guardians" : "Thomas Helm and Eva Jones"]
-
-let player15 = ["name" : "Les Clay", "soccerExperience" : "yes", "height" : "42", "guardians" : "Wynonna Brown"]
-
-let player16 = ["name" : "Herschel Krustofski", "soccerExperience" : "yes", "height" : "45", "guardians" : "Hyman and Rachel Krustofski"]
-
-let player17 = ["name" : "Chloe Alaska", "soccerExperience" : "no", "height" : "47", "guardians" : "David and Jamie Alaska"]
-
-let player18 = ["name" : "Joe Kavalier", "soccerExperience" : "no", "height" : "39", "guardians" : "Sam and Elaine Kavalier"]
+var playerArray = ["Joe Smith": [42, true, "Jim and Jan Smith"],
+                   "Jill Tanner": [36, true, "Clara Tanner"],
+                   "Bill Bon": [43, true, "Sara and Jenny Bon"],
+                   "Eva Gordon": [45, false, "Wendy and Mike Gordon"],
+                   "Matt Gill": [40, false, "Charles and Sylvia Gill"],
+                   "Kimmy Stein": [41, false, "Bill and Hillary Stein"],
+                   "Sammy Adams": [45, false, "Jeff Adams"],
+                   "Karl Saygan": [42, true, "Heather Biedsoe"],
+                   "Suzane Greenberg": [44, true, "Henrietta Dumas"],
+                   "Sal Dali": [41, false, "Gala Dali"],
+                   "Joe Kavalier": [39, false, "Sam and Elaine Kavalier"],
+                   "Ben Finkelstein": [44, false, "Aaron and Jill Finkelstein"],
+                   "Diego Soto": [41, true, "Robin and Sarika Soto"],
+                   "Chloe Alaska": [47, false, "David and Jamie Alaska"],
+                   "Arnold Willis": [43, false, "Claire Willis"],
+                   "Philip Helm": [44, true, "Thomas Helm and Eva Jones"],
+                   "Les Clay": [42, true, "Wynonna Brown"],
+                   "Herschel Krustofski": [45, true, "Hyman and Rachel Krustofski"]]
     
 
 
-// Next, create an empty collection variable to hold all the players’ data.
 
 
-// DATA FOR TOTAL PLAYERS
+// Team Variables. It's a variable if we decide to add more players to the roster
 
-var playerArray = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+var Sharks = [String: Array<AnyObject>]()
+var Dragons = [String: Array<AnyObject>]()
+var Raptors = [String: Array<AnyObject>]()
 
-
-
-
-
-
-// PART 2: Assign all 18 players to teams, experience level on all teams bust be the same. Store each teams player in its own new colllection variable.
-
-
-// Creating a function to sort players based on experienced and non-experienced players
-
-var experiencedSoccerPlayers = 0
-
-var unexperiencedSoccerPlayers = 0
-
-// This function is iterating through all 18 players to count how many of them are experienced
-
-
-
-func gettingPlayersExperience() {
-
-    for player in playerArray {
-        
-        // checking to see if the soccerExperience key returns yes
-    
-    if player["soccerExperience"] == "yes" {
-        
-        experiencedSoccerPlayers += 1
-        
-    } else {
-        
-        unexperiencedSoccerPlayers += 1
-        
-        
-    }
-    
-    
-}
-
-}
-   
-// Call the function
-
-gettingPlayersExperience()
-
-var experiencedPlayers = experiencedSoccerPlayers
-
-var nonexperiencedSoccerPlayers = unexperiencedSoccerPlayers
-
-let teams = ["Sharks", "Dragons", "Raptors"]
-
-// Team Variables
-
-var Sharks = [""]
-var Dragons = [""]
-var Raptors = [""]
 
 // Team Practice Constants
 
@@ -141,32 +70,65 @@ let sharksFirstGame = "March 17, 3PM"
 let dragonsFirstGame = "March 17, 1PM"
 let raptorsFirstGame = "March 18, 1PM"
 
-func assignPlayers() {
-    for player in playerArray {
-        
-        // This is saying if Sharks.count or players in the array is less than all of the players in the playerArray divided by 3 because it has to be split by 3 teams and the Sharks.count or players in the array / 2 is less than or equal to nonexperiencedSoccerPlayers divided by 3 and the Sharks.count or players in the array / 2 is less than or equal to the experiencedPlayers divided by 3 append the players amongst the Sharks team. 3 of each type
-        
-        if Sharks.count < playerArray.count / 3 && Sharks.count / 2 <= nonexperiencedSoccerPlayers / 3 && Sharks.count / 2 <= experiencedPlayers / 3 {
-            
-            Sharks.append("\(experiencedPlayers) + \(nonexperiencedSoccerPlayers)")
-       
-        
-        } else if Dragons.count < playerArray.count / 3 && Dragons.count / 2 <= nonexperiencedSoccerPlayers / 3 && Dragons.count / 2 <= experiencedPlayers / 3  {
-            
-            Dragons.append("\(experiencedPlayers) + \(nonexperiencedSoccerPlayers)")
 
-            
-        } else if Raptors.count < playerArray.count / 3 && Raptors.count / 2 <= nonexperiencedSoccerPlayers / 3 && Raptors.count / 2 <= experiencedPlayers / 3  {
-            
-            Raptors.append("\(experiencedPlayers) + \(nonexperiencedSoccerPlayers)")
 
+
+// PART 2: Assign all 18 players to teams, experience level on all teams bust be the same. Store each teams player in its own new colllection variable.
+
+
+// Function to count experienced/non-experienced players
+
+
+func sortPlayersExperiece() -> Int {
+    
+    var experiencedSoccerPlayers = 0
+    
+    for players in playerArray.values {
+        
+        if players[1] == true {
             
+            experiencedSoccerPlayers += 1
         }
     }
     
+    return experiencedSoccerPlayers
+    
 }
 
-assignPlayers()
+
+
+
+var numberOfExperiencedPlayers = sortPlayersExperiece()
+var numberOfInexperiencedPlayers = playerArray.count - numberOfExperiencedPlayers
+
+
+for (key, value) in playerArray {
+    
+    
+    if Sharks.count < playerArray.count / 3 && Sharks.count / 2 <= numberOfExperiencedPlayers / 3 && Sharks.count / <= numberOfInexperiencedPlayers / 3 {
+        
+        Sharks[key] = value
+        
+    } else if Dragons.count < playerArray.count / 3 && Dragons.count / 2 <= numberOfExperiencedPlayers / 3 && Dragons.count / <= numberOfInexperiencedPlayers / 3 {
+       
+        Dragons[key] = value
+
+        
+    } else if Raptors.count < playerArray.count / 3 && Raptors.count / 2 <= numberOfExperiencedPlayers / 3 && Raptors.count / <= numberOfInexperiencedPlayers / 3 {
+
+        Raptors[key] = value
+
+    }
+
+    
+}
+
+
+   
+
+
+
+
 
 
 
