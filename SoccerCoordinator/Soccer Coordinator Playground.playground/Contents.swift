291b51a6e0218ce42f5933be1dfd00b6e9359bc3
Nathanael Grant <nathandevelops@gmail.com>
2016-08-25 08:19:45 -0700

    Updating my printing letters function

--- SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
+++ SoccerCoordinator/Soccer Coordinator Playground.playground/Contents.swift
@@ -75,57 +75,57 @@ let player7 = ["name": "Sammy Adams",
 let player8 = ["name": "Karl Saygan",
                "height": 42,
                "experience": true,
-               "guardin": "Heather Bledsoe"]
+               "guardian": "Heather Bledsoe"]
 
 let player9 = ["name": "Suzane Greenberg",
                "height": 44,
                "experience": true,
-               "guardin": "Henrietta Dumas"]
+               "guardian": "Henrietta Dumas"]
 
 let player10 = ["name": "Sal Dali",
                "height": 41,
                "experience": false,
-               "guardin": "Gala Dali"]
+               "guardian": "Gala Dali"]
 
 let player11 = ["name": "Joe Kavalier",
                "height": 39,
                "experience": false,
-               "guardin": "Sam and Elaine Kavalier"]
+               "guardian": "Sam and Elaine Kavalier"]
 
 let player12 = ["name": "Ben Finkelstein",
                "height": 44,
                "experience": false,
-               "guardin": "Aaron and Jill Finkelstein"]
+               "guardian": "Aaron and Jill Finkelstein"]
 
 let player13 = ["name": "Diego Soto",
                "height": 41,
                "experience": true,
-               "guardin": "Robin and Sarika Soto"]
+               "guardian": "Robin and Sarika Soto"]
 
 let player14 = ["name": "Chloe Alaska",
                "height": 47,
                "experience": false,
-               "guardin": "David and Jamie Alaska"]
+               "guardian": "David and Jamie Alaska"]
 
 let player15 = ["name": "Arnold Willis",
                "height": 43,
                "experience": false,
-               "guardin": "Claire Willis"]
+               "guardian": "Claire Willis"]
 
 let player16 = ["name": "Phillip Helm",
                "height": 44,
                "experience": true,
-               "guardin": "Thomas Helm and Eva Jones"]
+               "guardian": "Thomas Helm and Eva Jones"]
 
 let player17 = ["name": "Les Clay",
                "height": 42,
                "experience": true,
-               "guardin": "Wynonna Brown"]
+               "guardian": "Wynonna Brown"]
 
 let player18 = ["name": "Herschel Krustofski",
                 "height": 45,
                 "experience": true,
-                "guardin": "Hyman and Rachel Krustofski"]
+                "guardian": "Hyman and Rachel Krustofski"]
 
 
 var playerArray = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
@@ -181,49 +181,54 @@ func countExperience() {
 
 print(experiencedPlayers)
 
-func sortPlayersExperiece() {
-
-for players in playerArray {
+func sortExperiencedPlayers() {
     
+    var index = 0
     
-    if sharks.count < playerArray.count / 3 && sharks.count / 2 <= experiencedPlayers / 3 && sharks.count / 2 <= inexperiencedPlayers / 3 {
+    while index < experiencedPlayers.count {
         
+        if sharks.count < 3 {
+            
+            sharks.append(experiencedPlayers[index])
+            
+        }else if raptors.count < 3 {
+            
+            raptors.append(experiencedPlayers[index])
+            
+        }else if dragons.count < 3 {
+            
+            dragons.append(experiencedPlayers[index])
+        }
         
-        
-    } else if dragons.count < playerArray.count / 3 && dragons.count / 2 <= experiencedPlayers / 3 && dragons.count / 2 <= inexperiencedPlayers / 3 {
-       
-
-        
-    } else if raptors.count < playerArray.count / 3 && raptors.count / 2 <= experiencedPlayers / 3 && raptors.count / 2 <= inexperiencedPlayers / 3 {
-
-
-    } else {
-        
-        print("Can't Assign The Player")
+        index += 1
     }
-    
- }
-
 }
+
+sortExperiencedPlayers()
+
+print("\(sharks)\n")
+print("\(dragons)\n")
+print("\(raptors)\n")
+
 
 func printPlayerLetters() {
     
     
     for individualPlayer in sharks {
         
-        print("Hello \(value[2]), your child \(key) is playing their first game with the Sharks! Join us on \(sharksFirstGame)")
+        print("Hello \(sharks["guardian"]), your child \(sharks["name"]) is playing their first game with the Sharks! Join us on \(sharksFirstGame)")
         
     }
     
     for individualPlayer in dragons {
        
-        print("Hello \(value[2]), your child \(key) is playing their first game with the Dragons! Join us on \(dragonsFirstGame)")
+        print("Hello \(dragons["guardian"]), your child \(dragons["name"]) is playing their first game with the Sharks! Join us on \(dragonsFirstGame)")
         
     }
     
     for individualPlayer in raptors {
        
-        print("Hello \(value[2]), your child \(key) is playing their first game with the Raptors! Join us on \(raptorsFirstGame)")
+        print("Hello \(raptors["guardian"]), your child \(raptors["name"]) is playing their first game with the Sharks! Join us on \(raptorsFirstGame)")
         
     }
 }
